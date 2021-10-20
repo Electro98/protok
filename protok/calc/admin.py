979f01e6dc1f57a9a5cec8836c7a4e32b101f081
Vladimir <voronov.volodya2013@yandex.ru>
2021-10-20 23:27:20 +0700

    настройки админки

--- protok/calc/admin.py
+++ protok/calc/admin.py
@@ -5,24 +5,29 @@ from . import models
 
 @admin.register(models.Transformer)
 class AuthorTransformer(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'manufacturer', 'power', 'transformer_type', 'price')
 
 
 @admin.register(models.HighVoltageDevice)
 class AuthorHighVoltageDevice(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'manufacturer', 'voltage', 'input_type', 'equipment_type', 'price')
+
+
+class SectionInline(admin.TabularInline):
+    model = models.Section
 
 
 @admin.register(models.LowVoltageDevice)
 class AuthorLowVoltageDevice(admin.ModelAdmin):
-    pass
+    inlines = [SectionInline]
+    list_display = ('name', 'manufacturer', 'voltage', 'input_type', 'input_device', 'price')
 
 
 @admin.register(models.Client)
 class AuthorClient(admin.ModelAdmin):
-    pass
+    list_display = ('full_name', 'email', 'phone_number')
 
 
 @admin.register(models.Order)
 class AuthorOrder(admin.ModelAdmin):
-    pass
+    list_display = ('client', 'transformer', 'lw_device', 'hw_device', 'create_date')
