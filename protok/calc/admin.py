3d25c2d34f701a8c00e00ec0fa2c6e576f997896
Vladimir <voronov.volodya2013@yandex.ru>
2021-10-20 19:04:35 +0700

    регистрация моделей в админке

--- protok/calc/admin.py
+++ protok/calc/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Transformer)
+class AuthorTransformer(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.HighVoltageDevice)
+class AuthorHighVoltageDevice(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.LowVoltageDevice)
+class AuthorLowVoltageDevice(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Client)
+class AuthorClient(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Order)
+class AuthorOrder(admin.ModelAdmin):
+    pass
